4be2f9fa1c535fece1f0b4d5a9fdcf76a81f8be7
Rafa Diaz <racrux@gmail.com>
2017-04-21 14:04:06 -0700

    give: Finished!

--- app/app.js
+++ app/app.js
@@ -1,27 +1,27 @@
-'use strict';
+"use strict";
 
-const path = require('path');
-const express = require('express');
-const config = require('./config');
+const path = require("path");
+const express = require("express");
+const config = require("./config");
 
 const app = express();
 
-app.disable('etag');
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-app.set('trust proxy', true);
+app.disable("etag");
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "jade");
+app.set("trust proxy", true);
 
 // Redirect root to /give
-app.get('/', (req, res) => {
-  res.redirect('/give');
+app.get("/", (req, res) => {
+  res.redirect("/give");
 });
 
-app.use('/give', require('./js/give/main'));
+app.use("/give", require("./js/give/main"));
 app.use(express.static("content"));
 
 // Basic 404 handler
 app.use((req, res) => {
-  res.status(404).send('Not Found');
+  res.status(404).send("Not Found");
 });
 
 // Basic error handler
@@ -29,14 +29,14 @@ app.use((err, req, res, next) => {
   console.error(err);
   // If our routes specified a specific response, then send that. Otherwise,
   // send a generic message so as not to leak anything.
-  res.status(500).send(err.response || 'Something broke!');
+  res.status(500).send(err.response || "Something broke!");
 });
 
 if (module === require.main) {
   // Start the server
-  const server = app.listen(config.get('PORT'), () => {
+  const server = app.listen(config.get("PORT"), () => {
     const port = server.address().port;
-    console.log(`App listening on port ${port}`);
+    console.log("App listening on port ${port}");
   });
 }
 
--- app/config.js
+++ app/config.js
@@ -1,32 +1,32 @@
-'use strict';
+"use strict";
 
-const nconf = module.exports = require('nconf');
-const path = require('path');
+const nconf = module.exports = require("nconf");
+const path = require("path");
 
 nconf
   .argv()
   .env([
-    'DATA_BACKEND',
-    'GCLOUD_PROJECT',
-    'INSTANCE_CONNECTION_NAME',
-    'MONGO_URL',
-    'MONGO_COLLECTION',
-    'MYSQL_USER',
-    'MYSQL_PASSWORD',
-    'NODE_ENV',
-    'PORT'
+    "DATA_BACKEND",
+    "GCLOUD_PROJECT",
+    "INSTANCE_CONNECTION_NAME",
+    "MONGO_URL",
+    "MONGO_COLLECTION",
+    "MYSQL_USER",
+    "MYSQL_PASSWORD",
+    "NODE_ENV",
+    "PORT"
   ])
-  .file({ file: path.join(__dirname, 'config.json') })
+  .file({ file: path.join(__dirname, "config.json") })
   .defaults({
-    DATA_BACKEND: 'datastore',
-    GCLOUD_PROJECT: 'cryptic-hawk-164803',
+    DATA_BACKEND: "datastore",
+    GCLOUD_PROJECT: "cryptic-hawk-164803",
     PORT: 8080
   });
 
-checkConfig('GCLOUD_PROJECT');
+checkConfig("GCLOUD_PROJECT");
 
 function checkConfig (setting) {
   if (!nconf.get(setting)) {
-    throw new Error(`You must set ${setting} as an environment variable or in config.json!`);
+    throw new Error("You must set ${setting} as an environment variable or in config.json!");
   }
 }
